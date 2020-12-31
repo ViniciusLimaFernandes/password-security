9f1bbb750ea484468a55959edd1c7b0bb023308b
ViniciusLimaFernandes <viniciuslima.4k@gmail.com>
2020-12-31 03:20:55 -0300

    Improving special character validation

--- src/main/java/com/password/security/application/domain/rules/HasAtLeastOneSpecialCharacter.java
+++ src/main/java/com/password/security/application/domain/rules/HasAtLeastOneSpecialCharacter.java
@@ -4,21 +4,37 @@ import com.password.security.application.domain.interfaces.PasswordRulesInterfac
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Slf4j
 @Component
 public class HasAtLeastOneSpecialCharacter implements PasswordRulesInterface {
+
+    private final String VALID_SPECIAL_CHARACTERS_REGEX = "!|@|#|$|%|^|&|*|(|)|\\-|+";
+
     @Override
     public boolean verify(String password) {
-        final String acceptedSpecialCharacters = "!|@|#|$|%|^|&|*|(|)|\\-|+";
-
-        final String SpecialCharactersBlacklist = "=|'|\"|\\\\||;|:|?|\\/|~|`|´|§|<|>|,|\\" +
-                ".|\\[|\\]|_|¨|¹|²|³|£|¢|¬|º|ª|°";
-
-        final String atLeastOneSpecialCharacterRegex =
-                "^(?=.*[" + acceptedSpecialCharacters + "])(?!.*[" + SpecialCharactersBlacklist + "]).*$";
-
         log.info("Checking if the password has at least one special character");
 
-        return password.matches(atLeastOneSpecialCharacterRegex);
+        final String hasAtLeastOneSpecialCharacterRegex = "^(?=.*[" + VALID_SPECIAL_CHARACTERS_REGEX + "]).*$";
+
+        if (haveAInvalidSpecialCharacter(password)) {
+            log.info("The password have an invalid special character.");
+            return false;
+        }
+
+        return password.matches(hasAtLeastOneSpecialCharacterRegex);
+    }
+
+    private boolean haveAInvalidSpecialCharacter(String password) {
+        final String specialCharactersBlacklistRegex = "(?=[\\p{P}\\p{S}])(?![" + VALID_SPECIAL_CHARACTERS_REGEX +
+                "])[\\w\\p{P}\\p{S}]";
+
+        Pattern pattern = Pattern.compile(specialCharactersBlacklistRegex);
+
+        Matcher matcher = pattern.matcher(password);
+
+        return matcher.find();
     }
 }
